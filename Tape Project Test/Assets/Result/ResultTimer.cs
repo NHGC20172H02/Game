98dbdc8c7312d998df9d04cc5f2bbd8130666a66
G016A1550\nakayama <alex.fuki@gmail.com>
2017-11-13 16:56:40 +0900

    タイトルシーン、ゲームプレイシーンでのUI、リザルトシーン追加

---
+++ Tape Project Test/Assets/Result/ResultTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ResultTimer : MonoBehaviour {
+
+    [SerializeField] int r_Count;
+    Text r_Text;
+
+    // Use this for initialization
+    void Start()
+    {
+        r_Text = this.GetComponent<Text>();
+    }
+
+    public void CountUp()
+    {
+        r_Count--;
+        r_Text.text = r_Count.ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CountUp();
+        if (r_Count <= 0)
+        {
+            r_Count = 0;
+        }
+
+        if (/*r_Count == 0 ||*/ Input.GetKeyDown(KeyCode.T))
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
