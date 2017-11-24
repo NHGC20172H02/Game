b7aa2a28fe1cfe61120c23c4edb3785f718e93cc
Nozawa <n.shota1115@gmail.com>
2017-11-24 15:30:01 +0900

    ネットの生成

--- Tape Project Test/Assets/TreeString/Net.cs
+++ Tape Project Test/Assets/TreeString/Net.cs
@@ -2,28 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StringUnit : Connecter {
+public class Net : Connecter {
 
 	public Connecter m_StartConnecter;
 	public Connecter m_EndConnecter;
 
-	public LineRenderer m_LineRenderer;
-	public CapsuleCollider m_Collider;
+	public MeshFilter m_MeshFilter;
+	public MeshCollider m_Collider;
 
-	public int m_Cost;
 	public StringShooter m_StringShooter;
 
-	public void SetLine(Vector3 Start, Vector3 End)
+	public void SetTriangle(Vector3 A, Vector3 B, Vector3 Corner)
 	{
-		m_LineRenderer.SetPositions(new Vector3[] { Start, End });
-		m_Collider.height = Vector3.Distance(Start, End);
-		m_Collider.center = new Vector3(0, 0, m_Collider.height * 0.5f);
+		var mesh = new Mesh();
+		mesh.vertices = new Vector3[]
+		{
+			A,B,Corner,
+			A,B,Corner
+
+		};
+		mesh.triangles = new int[]
+		{
+			0,1,2,
+			3,5,4
+		};
+		mesh.uv = new Vector2[]
+		{
+			new Vector2(0,1),
+			new Vector2(1,1),
+			new Vector2(0.5f,0),
+			new Vector2(0,1),
+			new Vector2(1,1),
+			new Vector2(0.5f,0)
+		};
+		mesh.RecalculateNormals();
+		m_MeshFilter.sharedMesh = mesh;
+		m_Collider.sharedMesh = mesh;
 	}
-	public void SetCost(int Cost)
-	{
-		m_Cost = Cost;
-	}
-	public void SetConnecter(Connecter Start,Connecter End)
+	public void SetConnecter(Connecter Start, Connecter End)
 	{
 		m_StartConnecter = Start;
 		m_EndConnecter = End;
@@ -36,7 +52,6 @@ public class StringUnit : Connecter {
 	{
 		if (m_SideNumber == sideNumber) return;
 		SetSide(sideNumber);
-		m_StringShooter.m_Strings.Remove(this);
 		m_StartConnecter.SideUpdate(sideNumber);
 		m_EndConnecter.SideUpdate(sideNumber);
 		foreach (var item in m_Child)
@@ -53,13 +68,12 @@ public class StringUnit : Connecter {
 		}
 		m_StartConnecter.RemoveString(this);
 		m_EndConnecter.RemoveString(this);
-		m_StringShooter.m_Cost -= m_Cost;
 		Destroy(gameObject);
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if(other.tag == "Player")
+		if (other.tag == "Player")
 		{
 			SideUpdate(other.GetComponentInParent<StringShooter>().m_SideNumber);
 		}
--- Tape Project Test/Assets/TreeString/StringShooter.cs
+++ Tape Project Test/Assets/TreeString/StringShooter.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class StringShooter : MonoBehaviour {
 
 	public float m_Radius;
-	public GameObject m_Prefab;
+	public GameObject m_StringUnit;
 	public int m_SideNumber;
 	public int m_MaxCost = 200;
 	public int m_Cost;
 	public List<StringUnit> m_Strings;
+	private Vector3 m_PreStartPoint;
+	private Vector3 m_PreEndPoint;
+	public GameObject m_Net;
+	public float m_NetAngleLimit = 40;
+	public float m_NetDistanceLimit = 0.01f;
 
 	// Use this for initialization
 	void Start () {
@@ -29,7 +34,7 @@ public class StringShooter : MonoBehaviour {
 		Connecter startConnecter = GetConnecter(start);
 		Connecter endConnecter = GetConnecter(end);
 		Quaternion look = Quaternion.LookRotation(end - start);
-		StringUnit stringUnit = Instantiate(m_Prefab, start, look).GetComponent<StringUnit>();
+		StringUnit stringUnit = Instantiate(m_StringUnit, start, look).GetComponent<StringUnit>();
 		stringUnit.m_StringShooter = this;
 		stringUnit.SetLine(start, end);
 		stringUnit.SetCost((int)Vector3.Distance(start, end));
@@ -43,6 +48,16 @@ public class StringShooter : MonoBehaviour {
 			m_Strings.RemoveAt(0);
 			firstStringUnit.Delete();
 		}
+		if(Vector3.Distance(start, m_PreEndPoint)< m_NetDistanceLimit && Vector3.Angle(end - start, m_PreStartPoint - start) < m_NetAngleLimit)
+		{
+			Net net = Instantiate(m_Net).GetComponent<Net>();
+			net.m_StringShooter = this;
+			net.SetTriangle(m_PreStartPoint, end, start);
+			net.SetSide(m_SideNumber);
+			net.SetConnecter(m_Strings[m_Strings.Count - 1], stringUnit);
+		}
+		m_PreEndPoint = end;
+		m_PreStartPoint = start;
 	}
 
 	private Connecter GetConnecter(Vector3 position)
