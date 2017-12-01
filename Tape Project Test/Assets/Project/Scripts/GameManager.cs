537d948e1af9b4f4adc915e83dc68d6f2d6faf93
Nozawa <n.shota1115@gmail.com>
2017-12-01 10:19:43 +0900

    マルチシーンソース追加

---
+++ Tape Project Test/Assets/Project/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager> {
+
+	[SerializeField]
+	private bool m_CursorVisible = false;
+	[SerializeField]
+	private CursorLockMode m_CursorLockMode = CursorLockMode.Locked;
+
+	[SerializeField]
+	private bool m_AFKActive = true;
+	[SerializeField]
+	private float m_AFKTime = 60;
+	private float m_Time;
+
+	[SerializeField]
+	private bool m_MultiSceneHotKeyActive = true;
+
+	// Use this for initialization
+	void Start () {
+		m_Time = 0;
+		Cursor.visible = m_CursorVisible;
+		Cursor.lockState = m_CursorLockMode;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) Exit();
+		HotKey();
+		AFK();
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+
+	private void HotKey()
+	{
+		if (m_MultiSceneHotKeyActive) return;
+		if (Input.GetKeyDown(KeyCode.Alpha0)) SceneController.Instance.ChangeScenes(0);
+		if (Input.GetKeyDown(KeyCode.Alpha1)) SceneController.Instance.ChangeScenes(1);
+		if (Input.GetKeyDown(KeyCode.Alpha2)) SceneController.Instance.ChangeScenes(2);
+		if (Input.GetKeyDown(KeyCode.Alpha3)) SceneController.Instance.ChangeScenes(3);
+		if (Input.GetKeyDown(KeyCode.Alpha4)) SceneController.Instance.ChangeScenes(4);
+		if (Input.GetKeyDown(KeyCode.Alpha5)) SceneController.Instance.ChangeScenes(5);
+		if (Input.GetKeyDown(KeyCode.Alpha6)) SceneController.Instance.ChangeScenes(6);
+		if (Input.GetKeyDown(KeyCode.Alpha7)) SceneController.Instance.ChangeScenes(7);
+		if (Input.GetKeyDown(KeyCode.Alpha8)) SceneController.Instance.ChangeScenes(8);
+		if (Input.GetKeyDown(KeyCode.Alpha9)) SceneController.Instance.ChangeScenes(9);
+	}
+	private void AFK()
+	{
+		if (m_AFKActive) return;
+		m_Time += Time.deltaTime;
+		if (Input.anyKey) m_Time = 0;
+		if (m_Time > m_AFKTime && SceneController.instance.GetActiveScenesIndex() != 0) SceneController.Instance.ChangeScenes(0);
+	}
+}
