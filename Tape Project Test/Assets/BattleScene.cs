7474e56a51cc36a82863d9755b13e21bd301d92f
Nozawa <n.shota1115@gmail.com>
2018-01-09 03:59:25 +0900

    演出実装

--- Tape Project Test/Assets/BattleScene.cs
+++ Tape Project Test/Assets/BattleScene.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class BattleScene : MonoBehaviour {
-	public Tree[] m_Trees;
+	public Animator m_UIAnimator;
 	public float m_Timer = 180;
 	public Text m_TimerUI;
 
     enum GameState
     {
+		Ready,
         Play,
         Result,
     }
@@ -18,38 +19,34 @@ public class BattleScene : MonoBehaviour {
     GameState state = GameState.Play;
 
     // Use this for initialization
-    void Start ()
+    IEnumerator Start ()
     {
-        
-    }
-	
+		state = GameState.Ready;
+		PauseManager.Instance.Pause(false);
+
+		yield return new WaitForSeconds(2.0f);
+
+		state = GameState.Play;
+		PauseManager.Instance.Pause(true);
+
+		yield return new WaitUntil(() => (m_Timer <= 0.0f));
+
+		PauseManager.Instance.Pause(false);
+		m_UIAnimator.SetTrigger("Finish");
+
+		yield return new WaitForSeconds(2.0f);
+
+		state = GameState.Result;
+		SceneManager.LoadScene("Result", LoadSceneMode.Additive);
+	}
+
 	// Update is called once per frame
 	void Update ()
     {
         if (state == GameState.Play)
         {
-            if (Input.GetKeyDown(KeyCode.Return) /*|| m_Timer <= 0*/)
-            {
-                int score = 0;
-                int scoreB = 0;
-                foreach (var item in m_Trees)
-                {
-                    score += item.m_SideNumber == 1 ? 1 : 0;
-                    scoreB += item.m_SideNumber == 2 ? 1 : 0;
-                }
-                Sample.score = score;
-                Sample.scoreB = scoreB;
-
-                //SceneManager.LoadScene("Result", LoadSceneMode.Additive);
-            }
             m_TimerUI.text = ((int)m_Timer).ToString();
             m_Timer -= Time.deltaTime;
-
-            if (m_Timer < 0)
-            {
-                state = GameState.Result;
-                SceneManager.LoadScene("Result", LoadSceneMode.Additive);
-            }
         }
     }
 }
