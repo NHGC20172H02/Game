593def22de56c191ac89fe69e4e6437ff3cb5803
G016A1550\nakayama <alex.fuki@gmail.com>
2018-01-09 15:36:03 +0900

    再度ポーズ画面追加

---
+++ Tape Project Test/Assets/PauseUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+    public GameObject m_PausePanel;
+    public GameObject m_PauseBackGround;
+    public GameObject m_ReturnToGame;
+    public GameObject m_ReturnToTitle;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_PausePanel.SetActive(false);
+        m_PauseBackGround.SetActive(false);
+        m_ReturnToGame.SetActive(false);
+        m_ReturnToTitle.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("Start"))
+        {
+            m_PausePanel.SetActive(true);
+            m_PauseBackGround.SetActive(true);
+            m_ReturnToGame.SetActive(true);
+            m_ReturnToTitle.SetActive(true);
+
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(m_ReturnToGame);
+        }
+    }
+}
