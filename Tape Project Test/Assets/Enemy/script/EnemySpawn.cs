98b4815b79ad4a6c4cdbf8634f10032ef684dcda
HOME-PC\tomohiro <tom0430@docomo.ne.jp>
2018-01-12 10:39:50 +0900

    EnemySpawn(仮)

---
+++ Tape Project Test/Assets/Enemy/script/EnemySpawn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject Enemy_E;
+    public GameObject Enemy_N;
+    public GameObject Enemy_H;
+
+    GameObject m_EnemyAI;
+    GameObject spawnEnemy;
+
+    int s;
+    bool isStarted = false;
+
+    // Use this for initialization
+    void Start () {
+        isStarted = true;
+        DontDestroyOnLoad(this);
+
+        m_EnemyAI = SpaenEnemy(s);
+
+        Instantiate(m_EnemyAI, this.transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject SpaenEnemy(int spawn)
+    {
+        if(spawn == 1)
+        {
+            spawnEnemy = Enemy_H;
+        }
+        if(spawn == 2)
+        {
+            spawnEnemy = Enemy_N;
+        }
+        if(spawn == 3)
+        {
+            spawnEnemy = Enemy_E;
+        }
+
+        return spawnEnemy;
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (isStarted)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Tape Project Test/Assets/Enemy/script/SpawnTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTest : MonoBehaviour {
+    public int s;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<EnemySpawn>().SpaenEnemy(s);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
