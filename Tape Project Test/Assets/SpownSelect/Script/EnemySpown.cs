b7e08e6ba41340def37146ffdc80878f71498928
G016A1550\nakayama <alex.fuki@gmail.com>
2018-01-30 15:50:14 +0900

    難易度に応じて1本の木が自分のものか敵のものになる

---
+++ Tape Project Test/Assets/SpownSelect/Script/EnemySpown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpown : MonoBehaviour
+{
+    public static int enemySpownData;
+    public int num;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    //public void OnClick()
+    //{
+    //    enemySpownData = num;
+    //}
+}
