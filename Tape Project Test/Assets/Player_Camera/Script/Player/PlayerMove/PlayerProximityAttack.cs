bdd7d2e6c0aaf821bd821e11668db431dc88c033
sugi23 <sugi23l4game@outlook.jp>
2018-01-30 21:41:37 +0900

    カーソルアイコン(仮)等を追加

---
+++ Tape Project Test/Assets/Player_Camera/Script/Player/PlayerMove/PlayerProximityAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class Player {
+
+	void ProximityAttack()
+    {
+        Collider[] cols = Physics.OverlapBox(m_center, new Vector3(0.5f, 0.3f, 0.5f), Quaternion.identity, m_EnemyLayer);
+        foreach(Collider col in cols)
+        {
+            //SendingBodyBlow(col.gameObject);
+            Debug.Log("hit");
+        }
+
+        if (m_Animator.GetCurrentAnimatorStateInfo(0).IsName("GroundMove"))
+            m_StateManager.StateProcassor.State = m_StateManager.TreeTp;
+    }
+}
