53663a0b312850b3ba7a225f622f50ef5ad3f28d
Nozawa <n.shota1115@gmail.com>
2018-01-31 18:11:15 +0900

    ゲージ修正

--- Tape Project Test/Assets/Project/UI/Gauge/TerritoryGaugeUI.cs
+++ Tape Project Test/Assets/Project/UI/Gauge/TerritoryGaugeUI.cs
@@ -6,12 +6,9 @@ using UnityEngine.UI;
 public class TerritoryGaugeUI : MonoBehaviour
 {
 	public Image m_GaugeIn;
-	public Image m_GaugeOver;
 
 	public Sprite m_GaugeBlue;
-	public Sprite m_GaugeBlueOver;
 	public Sprite m_GaugeRed;
-	public Sprite m_GaugeRedOver;
 
 	public void SetTree(Tree tree)
 	{
@@ -20,15 +17,12 @@ public class TerritoryGaugeUI : MonoBehaviour
 		if (rate >= 0)
 		{
 			m_GaugeIn.sprite = m_GaugeBlue;
-			m_GaugeOver.sprite = m_GaugeBlueOver;
 		}
 		else
 		{
 			m_GaugeIn.sprite = m_GaugeRed;
-			m_GaugeOver.sprite = m_GaugeRedOver;
 		}
 		float fillAmount = Mathf.Abs(rate) * 0.01f;// 0~100 -> 0~1
 		m_GaugeIn.fillAmount = fillAmount;
-		m_GaugeOver.fillAmount = fillAmount - 1;
 	}
 }
