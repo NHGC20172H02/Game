b5175933618699eb854f3ebf1abc2c17a043a583
G016A1550\nakayama <alex.fuki@gmail.com>
2018-02-15 05:08:26 +0900

    プレイヤーのスポーン生成の位置修正、敵と木と被りを修正

--- Tape Project Test/Assets/SpownSelect/Script/SpownPoint.cs
+++ Tape Project Test/Assets/SpownSelect/Script/SpownPoint.cs
@@ -8,7 +8,7 @@ public class SpownPoint : MonoBehaviour
 	void Start ()
     {
         //PlayerSpownで決めた座標をバトルシーンの座標に変換
-        Vector3 spownPosition = new Vector3 (PlayerSpown.spownPos.x * 0.5f, 0 ,PlayerSpown.spownPos.y * 0.5f);
+        Vector3 spownPosition = new Vector3 (PlayerSpown.spownPos.x * 0.5f, 0 ,PlayerSpown.spownPos.y * 0.6f);
         GameObject.Find("PlayerCamera").transform.position = spownPosition;
 
         var playerRotate = this.gameObject.transform.eulerAngles;
