c5c21a038c4848bd3e036131607b73c04ac711a2
Nozawa <n.shota1115@gmail.com>
2018-02-15 14:54:17 +0900

    コミット

--- Tape Project Test/Assets/Project/Select/IconAnimation.cs
+++ Tape Project Test/Assets/Project/Select/IconAnimation.cs
@@ -3,26 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpawnSelect : MonoBehaviour {
+public class IconAnimation : MonoBehaviour {
 
-	
+	public Image m_Circle;
+	public Image m_Line;
+	public Image m_Arrow;
 
+	public Vector3 m_PlayerPos;
 	// Use this for initialization
-	void Start () {
-		
+	IEnumerator Start () {
+		yield return null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
-
-	public void StartSelected()
-	{
-		StartCoroutine(Selected());
-	}
-	public IEnumerator Selected()
-	{
-		yield return null;
 	}
 }
